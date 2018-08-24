4813377d581b6f6976370533ba7ef917b587622f
Guilherme Augusto <guilherme_amsilva@hotmail.com>
2018-08-24 18:13:13 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+
+const app = express();
+const router = express.Router();
+
+mongoose.connect('mongodb://guilherme:gui2812@ds018498.mlab.com:18498/nodestore')
+
+const Product = require('./models/product');
+
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+module.exports = app;
